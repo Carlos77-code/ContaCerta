af2fe758214e5a1b1aff8b9c390dc651bc1abedb
Carlos77-code <carloslimafernandes@hotmail.com>
2024-01-06 09:29:57 -0300

    Controle de despesas iniciado

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ContaCerta.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContaCerta.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DespesasModel> Despesas { get; set; }
+    }
+}
---
+++ Models/DespesasModel.cs
@@ -0,0 +1,27 @@
+﻿namespace ContaCerta.Models
+{
+    public class DespesasModel
+    {
+        public int Id { get; set; }
+
+        public string Categoria { get; set; }
+
+        public string Descricao { get; set; }
+
+        public double ValorDaParcela { get; set; }
+
+        public string StatusDoMes { get; set; }
+
+        public string FormaDePagamento { get; set; }
+
+        public int QuantidadeParcelasPagas { get; set; }
+
+        public int QuantiadeDeParcelasFaltantes { get; set; }
+
+        public int QuantidadeTotalDeParcelas { get; set; }
+
+        public double ValorTotalAPagar { get; set; }
+
+        public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
+    }
+}
