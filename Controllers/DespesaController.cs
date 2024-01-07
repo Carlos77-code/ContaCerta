89569b7a1b33a0de1b84513a7f8216e3d48c8e16
Carlos77-code <carloslimafernandes@hotmail.com>
2024-01-06 22:05:21 -0300

    Criada primeira tabela e exibição na página

---
+++ Controllers/DespesaController.cs
@@ -0,0 +1,21 @@
+﻿using ContaCerta.Data;
+using ContaCerta.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContaCerta.Controllers
+{
+    public class DespesaController : Controller
+    {
+        readonly private ApplicationDbContext _db;
+        public DespesaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<DespesasModel> despesas = _db.Despesas;
+            return View(despesas);
+        }
+    }
+}
